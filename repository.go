3bd1950e5555281f29c4cbd4d74c42d033786f2c
BPeters58 <benjamin.peters58@t-mobile.com>
2020-11-04 14:11:40 -0500

    repo fixes

--- repository.go
+++ repository.go
@@ -20,46 +20,36 @@ func AddRepository(repo interface{}, typ interface{}) {
 	for i := 0; i < typeOf.NumField(); i++ {
 		if typeOf.Field(i).Name != "CRUDRepository" {
 			// ...
+			query := typeOf.Field(i).Tag.Get("query")
+			if query != "" {
+				// // the implementation passed to MakeFunc.
+				// // It must work in terms of reflect.Values so that it is possible
+				// // to write code without knowing beforehand what the types
+				// // will be.
+				// implemention := func(in []reflect.Value) []reflect.Value {
+				// 	inVal := []interface{}{}
+				// 	for _, val := range in {
+				// 		inVal = append(inVal, val.Interface())
+				// 	}
+				// 	result, err := app.db.Exec(query, inVal...)
+				// 	if err != nil {
+				// 		return []reflect.Value{reflect.ValueOf(nil), reflect.ValueOf(err)}
+				// 	}
+				// 	return []reflect.Value{reflect.ValueOf(result), reflect.ValueOf(nil)}
+				// }
+				// // fptr is a pointer to a function.
+				// // Obtain the function value itself (likely nil) as a reflect.Value
+				// // so that we can query its type and then set the value.
+				// fn := obj.Field(i)
+
+				// // Make a function of the right type.
+				// v := reflect.MakeFunc(fn.Type(), implemention)
+
+				// // Assign it to the value fn represents.
+				// fn.Set(v)
+			}
 		}
 	}
-}
-
-func addFunction() {
-	// swap is the implementation passed to MakeFunc.
-	// It must work in terms of reflect.Values so that it is possible
-	// to write code without knowing beforehand what the types
-	// will be.
-	swap := func(in []reflect.Value) []reflect.Value {
-		return []reflect.Value{in[1], in[0]}
-	}
-
-	// makeSwap expects fptr to be a pointer to a nil function.
-	// It sets that pointer to a new function created with MakeFunc.
-	// When the function is invoked, reflect turns the arguments
-	// into Values, calls swap, and then turns swap's result slice
-	// into the values returned by the new function.
-	makeSwap := func(fptr interface{}) {
-		// fptr is a pointer to a function.
-		// Obtain the function value itself (likely nil) as a reflect.Value
-		// so that we can query its type and then set the value.
-		fn := reflect.ValueOf(fptr).Elem()
-
-		// Make a function of the right type.
-		v := reflect.MakeFunc(fn.Type(), swap)
-
-		// Assign it to the value fn represents.
-		fn.Set(v)
-	}
-
-	// Make and call a swap function for ints.
-	var intSwap func(int, int) (int, int)
-	makeSwap(&intSwap)
-	fmt.Println(intSwap(0, 1))
-
-	// Make and call a swap function for float64s.
-	var floatSwap func(float64, float64) (float64, float64)
-	makeSwap(&floatSwap)
-	fmt.Println(floatSwap(2.72, 3.14))
 }
 
 type CRUDRepository struct{}
@@ -68,7 +58,7 @@ func (c CRUDRepository) Save(model interface{}) error {
 	log.Println("[Save]: starting database entry")
 	typeOf := reflect.TypeOf(dataType)
 	valueOfDataType := reflect.ValueOf(dataType)
-	valueOf := reflect.ValueOf(model)
+	valueOf := reflect.Indirect(reflect.ValueOf(model))
 	tableName := strings.ToLower(typeOf.Name())
 	if valueOfDataType.NumMethod() != 0 {
 		tableMethod := valueOfDataType.MethodByName("TableName")
@@ -77,25 +67,24 @@ func (c CRUDRepository) Save(model interface{}) error {
 			tableName = tableNameValue[0].Interface().(string)
 		}
 	}
-	sqlStatement := `INSERT INTO ` + tableName + ` (something) VALUES (`
-	numOfFields := typeOf.NumField()
+
 	fieldValues := []interface{}{}
+	fieldNames := []string{}
+	fieldMarkers := []string{}
+	numOfFields := typeOf.NumField()
 	for i := 0; i < numOfFields; i++ {
 		// check if field is ID serial
 		field := typeOf.Field(i)
 		id := field.Tag.Get("id")
 		if id != "" {
-			log.Println(id)
 			continue
 		}
 		fieldValues = append(fieldValues, valueOf.Field(i).Interface())
-		if (i + 1) == numOfFields {
-			sqlStatement += "$" + strconv.FormatInt(int64(i), 10)
-		} else {
-			sqlStatement += "$" + strconv.FormatInt(int64(i), 10) + ","
-		}
+		jsonTag := field.Tag.Get("json")
+		fieldNames = append(fieldNames, jsonTag)
+		fieldMarkers = append(fieldMarkers, "$"+strconv.FormatInt(int64(i), 10))
 	}
-	sqlStatement += ");"
+	sqlStatement := "INSERT INTO " + tableName + " (" + strings.Join(fieldNames, ",") + ") VALUES ( " + strings.Join(fieldMarkers, ",") + " );"
 	log.Println("[Save]: saving ", fieldValues)
 	_, err := app.db.Exec(sqlStatement, fieldValues...)
 	if err != nil {
@@ -111,8 +100,8 @@ func (c CRUDRepository) FindByID(id string) (map[string]interface{}, error) {
 	valueOfDataType := reflect.ValueOf(dataType)
 	tableName := strings.ToLower(typeOf.Name())
 	if valueOfDataType.NumMethod() != 0 {
-		tableMethod := valueOfDataType.MethodByName("Table")
-		if !tableMethod.IsNil() {
+		tableMethod := valueOfDataType.MethodByName("TableName")
+		if tableMethod.IsValid() {
 			tableNameValue := tableMethod.Call([]reflect.Value{})
 			tableName = tableNameValue[0].Interface().(string)
 		}
